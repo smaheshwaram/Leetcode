d6f614e0f60acf09c50b96b7426282d14d1377e0
sugandhi <smaheshwaram1@gmail.com>
2019-05-27 17:55:45 -0500

    commit 3, easy solution for Lemonade change, leetcode easy

--- src/leetcodeeasy/LemonadeChange.java
+++ src/leetcodeeasy/LemonadeChange.java
@@ -80,4 +80,19 @@ public class LemonadeChange {
         }
         return true;
     }
+
+    //easy java concise solution
+    public boolean lemonadeChange2(int[] bills) {
+        int five = 0, ten = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) five++;
+            else if (bill == 10) {five--; ten++;}
+            else if (bill == 20 && ten > 0) {ten--; five--;}
+            else five -= 3;
+
+            if (five < 0) return false;
+        }
+        return true;
+    }
 }
